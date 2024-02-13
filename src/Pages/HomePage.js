3284ac2f57062a00bcbf54fd79a96a965f31cd1b
khagendrajora <khagijora2074@gmail.com>
2024-02-14 00:27:47 +0545

    edited

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import PersonalDetails from '../Components/PersonalDetails'
 import '../Components/Style.css'
+
 import { Experience } from '../Components/Experience'
 import Projects from '../Components/Projects'
 import ContactMe from '../Components/ContactMe'
-import '../Components/ResponsiveStyle.css'
+
 
 
 export const HomePage = () => {
