64ef7405a05875e55912ba55c3549d4b6fd90441
khagendrajora <khagijora2074@gmail.com>
2024-06-23 21:12:09 +0545

    edt

--- src/Components/PersonalDetails.js
+++ src/Components/PersonalDetails.js
@@ -15,8 +15,8 @@ const PersonalDetails = () => {
                     <img src="Assets/kj.JPG" alt="khagendra" style={{ width: "20rem" }} />
                 </div>
                 <div className="Right">
-                    I believe myself as a self-motivated and tech enthusiastic person. Always ready to face challenges.
-                    Have a good experience in developer field. Eager to learn data.
+                    I believe myself as a self-motivated and tech enthusiastic. Always ready to face challenges.
+                    Working as a developer.
                     <button id='btn' onClick={Resume}>My Resume</button>
                 </div>
             </div>
